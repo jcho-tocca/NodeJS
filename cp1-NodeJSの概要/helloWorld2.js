5af721c728ebb310612d73fbd977bcc03a357c33
jcho-tocca <jaeman.cho@tocca-net.jp>
2021-11-15 12:41:43 +0900

    cp1

--- cp1-NodeJSの概要/helloWorld2.js
+++ cp1-NodeJSの概要/helloWorld2.js
@@ -1,4 +1,4 @@
-var http = require('http');
+let http = require('http');
 http.createServer(function(request, response) {
 	response.writeHead(200, {'Content-Type':'text/html'});
 	response.end('<h1>Hello World!</h1>');
